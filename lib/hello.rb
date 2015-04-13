69ae8c0218a693dc4587be0a748bab47c51d726e
Kazushi Kawamura <wakaka2277@gmail.com>
2015-04-13 14:41:04 -0400

    Hello uss Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,9 @@
+require './greeter.rb'
+
 # Default is World
 # Author: Kazushi Kawamura (wakaka2277@gmail.com)
 name = ARGV.first || "World"
 puts "Hello, #{name}!"
+
+greeter = Greeter.new(name)
+puts greeter.greet
